61110d46e7835482c62d0a7aeeb2c69e5d2f8c32
gcaron00 <gcaron@live.unc.edu>
2022-04-07 14:36:30 -0400

    first commit

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ const help = args.help
 const debug = args.debug
 const port = args.port || 5555
 
-if(log == true){
-    const writestream  = fs.createWriteStream('access.log', { flags: 'a' })
-    app.use(morgan('combined', { stream: writestream  }))
+if(log == "true"){
+    const WRITESTREAM  = fs.createWriteStream('access.log', { flags: 'a' })
+    app.use(morgan('combined', { stream: WRITESTREAM }))
 }
 
 if (help) {
