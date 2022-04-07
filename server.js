9c4e2cb90c380ddf91831ed5c7acf32cb069764a
gcaron00 <gcaron@live.unc.edu>
2022-04-07 15:24:36 -0400

    first commit

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ app.use( (req, res, next) => {
     const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr, 
         remoteuser, time, method, url, protocol, httpversion, secure, 
         status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, 
-        logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.secure, 
-        logdata.status, logdata.referer, logdata.useragent)
+    const info = stmt.run(String(logdata.remoteaddr), String(logdata.remoteuser), String(logdata.time), 
+        String(logdata.method), String(logdata.url), String(logdata.protocol), String(logdata.httpversion), String(logdata.secure), 
+        String(logdata.status), String(logdata.referer), String(logdata.useragent))
     res.status(200).json(info)
 })
 
